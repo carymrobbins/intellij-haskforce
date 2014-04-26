0132a6323babdf029e7d1df9e9637d4916fe3396
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-04-26 19:23:52 +0200

    Add a test case for #10.

---
+++ tests/gold/parser/String00002.hs
@@ -0,0 +1,7 @@
+module String00002 where
+
+s = "\
+    \Remove any backslash (except the first one) and put it back.\
+    \The syntax highlighting will break!\
+    \To fix, modify the first line of this string literal.\
+\"
