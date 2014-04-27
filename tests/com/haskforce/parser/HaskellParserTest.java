5a9afbab545a4eb869f118943de3c996e4bbcaad
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-04-27 05:49:28 +0200

    Overapproximate string literals. Fixes #10.

--- tests/com/haskforce/parser/HaskellParserTest.java
+++ tests/com/haskforce/parser/HaskellParserTest.java
@@ -37,6 +37,7 @@ public class HaskellParserTest extends HaskellParserTestBase {
     public void testLambda00001()       { doTest(true, false); }
     public void testPragma00001()       { doTest(true, false); }
     public void testString00001()       { doTest(true, false); }
+    public void testString00002()       { doTest(true, false); }
     public void testTempHask00001()     { doTest(true, false); }
     public void testQuote00001()        { doTest(true, false); }
 }
