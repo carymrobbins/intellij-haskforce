9f35e2c1fb634974d97bdd7253583a5cc5206bc0
Cary Robbins <carymrobbins@gmail.com>
2014-04-30 02:09:23 -0500

    Removed dependency on patched GeneratedParserUtilBase.

--- gen/com/haskforce/parser/HaskellParser.java
+++ gen/com/haskforce/parser/HaskellParser.java
@@ -5,7 +5,7 @@ import com.intellij.lang.PsiBuilder;
 import com.intellij.lang.PsiBuilder.Marker;
 import com.intellij.openapi.diagnostic.Logger;
 import static com.haskforce.psi.HaskellTypes.*;
-import static org.intellij.grammar.parser.GeneratedParserUtilBase.*;
+import static com.intellij.lang.parser.GeneratedParserUtilBase.*;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.tree.TokenSet;
