9ef61de4e790856ca29616a08d60c54671180e06
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-04-30 02:29:32 +0200

    Remove setHighlightingRecursive and simplify the body of setHighlighting. Fixes #14.

--- src/com/haskforce/HaskellAnnotator.java
+++ src/com/haskforce/HaskellAnnotator.java
@@ -17,43 +17,43 @@ public class HaskellAnnotator implements Annotator {
             @Override
             public void visitNcomment(@NotNull HaskellNcomment o) {
                 super.visitNcomment(o);
-                setHighlightingRecursive(o, holder, HaskellSyntaxHighlighter.NCOMMENT);
+                setHighlighting(o, holder, HaskellSyntaxHighlighter.NCOMMENT);
             }
 
             @Override
             public void visitPragma(@NotNull HaskellPragma o) {
                 super.visitPragma(o);
-                setHighlightingRecursive(o, holder, HaskellSyntaxHighlighter.PRAGMA);
+                setHighlighting(o, holder, HaskellSyntaxHighlighter.PRAGMA);
             }
 
             @Override
             public void visitQvarid(@NotNull HaskellQvarid o) {
                 super.visitQvarid(o);
-                setHighlightingRecursive(o, holder, HaskellSyntaxHighlighter.VARID);
+                setHighlighting(o, holder, HaskellSyntaxHighlighter.VARID);
             }
 
             @Override
             public void visitQinfixvarid(@NotNull HaskellQinfixvarid o) {
                 super.visitQinfixvarid(o);
-                setHighlightingRecursive(o, holder, HaskellSyntaxHighlighter.INFIXVARID);
+                setHighlighting(o, holder, HaskellSyntaxHighlighter.INFIXVARID);
             }
 
             @Override
             public void visitQvarsym(@NotNull HaskellQvarsym o) {
                 super.visitQvarsym(o);
-                setHighlightingRecursive(o, holder, HaskellSyntaxHighlighter.VARSYM);
+                setHighlighting(o, holder, HaskellSyntaxHighlighter.VARSYM);
             }
 
             @Override
             public void visitQconsym(@NotNull HaskellQconsym o) {
                 super.visitQconsym(o);
-                setHighlightingRecursive(o, holder, HaskellSyntaxHighlighter.CONSYM);
+                setHighlighting(o, holder, HaskellSyntaxHighlighter.CONSYM);
             }
 
             @Override
             public void visitPstringtoken(@NotNull HaskellPstringtoken o) {
                 super.visitPstringtoken(o);
-                setHighlightingRecursive(o, holder, HaskellSyntaxHighlighter.STRING);
+                setHighlighting(o, holder, HaskellSyntaxHighlighter.STRING);
             }
 
             @Override
@@ -66,16 +66,7 @@ public class HaskellAnnotator implements Annotator {
 
     private static void setHighlighting(@NotNull PsiElement element, @NotNull AnnotationHolder holder,
                                         @NotNull TextAttributesKey key) {
-        holder.createInfoAnnotation(element, null).setEnforcedTextAttributes(TextAttributes.ERASE_MARKER);
         holder.createInfoAnnotation(element, null).setEnforcedTextAttributes(
                 EditorColorsManager.getInstance().getGlobalScheme().getAttributes(key));
-    }
-
-    private static void setHighlightingRecursive(@NotNull PsiElement element, @NotNull AnnotationHolder holder,
-                                                 @NotNull TextAttributesKey key) {
-        setHighlighting(element, holder, key);
-        for (PsiElement child : element.getChildren()) {
-            setHighlightingRecursive(child, holder, key);
-        }
     }
 }
