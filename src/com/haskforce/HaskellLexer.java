67f02cbdcfe9cf50805235ee1e3bc50ec6ea636e
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-05-02 11:53:50 +0200

    Nuke the unused lexer and add instructions on how to generate the new lexer.

--- src/com/haskforce/HaskellLexer.java
+++
@@ -1,9 +0,0 @@
-package com.haskforce;
-
-import com.intellij.lexer.FlexAdapter;
-
-public class HaskellLexer extends FlexAdapter {
-    public HaskellLexer() {
-        super(new _HaskellLexer());
-    }
-}
