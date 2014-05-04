2c9c0f01322657177e46d23395929eda64d4e1e2
Cary Robbins <carymrobbins@gmail.com>
2014-05-04 14:35:38 -0500

    Cleaned up messages for tools/generate.sh

--- tools/generate.sh
+++ tools/generate.sh
@@ -6,6 +6,10 @@ echo_loud () {
     echo "$(tput setaf 2 && tput bold)$@$(tput sgr 0)"
 }
 
+echo_bad () {
+    echo "$(tput setaf 1 && tput bold)$@$(tput sgr 0)"
+}
+
 DIR=$(dirname $0)
 PROJECT_DIR=$DIR/..
 GEN_DIR=$PROJECT_DIR/gen
@@ -14,11 +18,12 @@ FLEX_FILE=$PROJECT_DIR/src/com/haskforce/highlighting/_HaskellSyntaxHighlighting
 
 if ([ ! -f $DIR/.jflex ] || [ ! -f $DIR/.jflex-skeleton ] ||
     [ ! -f $DIR/.lib ] || [ ! -f $DIR/.grammar-kit ]); then
-    echo "Must set paths in $DIR/.flex, $DIR/.jflex-skeleton, $DIR/.lib, $DIR/.grammar-kit"
-    echo "Something like:"
-    echo "  $ echo ~/build/idea/tools/lexer/jflex-1.4/bin/jflex > $DIR/.jflex"
-    echo "  $ echo ~/build/idea/tools/lexer/idea-flex.skeleton > $DIR/.jflex-skeleton"
-    echo "  $ echo ~/Library/Application Support/IdeaIC13/GrammarKit/lib/grammar-kit.jar > $DIR/.grammar-kit"
+    echo_bad "Must set paths in $DIR/.flex, $DIR/.jflex-skeleton, $DIR/.lib, $DIR/.grammar-kit"
+    echo_bad "Note that these paths must be absolute - e.g. use /Users/crobbins instead of ~"
+    echo_bad "Something like (but may be different for you):"
+    echo "  $ echo /Users/crobbins/build/idea/tools/lexer/jflex-1.4/bin/jflex > $DIR/.jflex"
+    echo "  $ echo /Users/crobbins/build/idea/tools/lexer/idea-flex.skeleton > $DIR/.jflex-skeleton"
+    echo "  $ echo /Users/crobbins/Library/Application Support/IdeaIC13/GrammarKit/lib/grammar-kit.jar > $DIR/.grammar-kit"
     echo "  $ echo /Applications/IntelliJ IDEA 13 CE.app/lib > $DIR/.lib"
     exit 1
 fi
@@ -28,7 +33,7 @@ JFLEX_SKELETON="$(cat $DIR/.jflex-skeleton)"
 LIB="$(cat $DIR/.lib)"
 GRAMMAR_KIT="$(cat  $DIR/.grammar-kit)"
 
-echo_loud "Deleting old gen directory $PROJECT_DIR/gen ..."
+echo_loud "Deleting old gen directory ..."
 rm -rf $PROJECT_DIR/gen
 
 echo_loud "Running JFlex ..."
