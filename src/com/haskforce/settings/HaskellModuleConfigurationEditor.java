7fa14a3a03dc68d63c0be13f9a7b0d16a1b5b508
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-05-06 20:31:02 +0200

    Add a module configuration panel.

---
+++ src/com/haskforce/settings/HaskellModuleConfigurationEditor.java
@@ -0,0 +1,28 @@
+package com.haskforce.settings;
+
+import com.haskforce.HaskellModuleType;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleConfigurationEditor;
+import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.roots.ui.configuration.ClasspathEditor;
+import com.intellij.openapi.roots.ui.configuration.DefaultModuleEditorsProvider;
+import com.intellij.openapi.roots.ui.configuration.JavaContentEntriesEditor;
+import com.intellij.openapi.roots.ui.configuration.ModuleConfigurationState;
+import com.intellij.openapi.roots.ui.configuration.OutputEditor;
+
+/**
+ * Project Settings->Module panel.
+ */
+public class HaskellModuleConfigurationEditor extends DefaultModuleEditorsProvider {
+    public ModuleConfigurationEditor[] createEditors(ModuleConfigurationState state) {
+        Module module = state.getRootModel().getModule();
+        if (!(ModuleType.get(module) instanceof HaskellModuleType)) {
+            return ModuleConfigurationEditor.EMPTY;
+        }
+        return new ModuleConfigurationEditor[]{
+                new JavaContentEntriesEditor(module.getName(), state),
+                // new OutputEditor(state), TODO: Add when we have Haddock support.
+                new ClasspathEditor(state),
+        };
+    }
+}
