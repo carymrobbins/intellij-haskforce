11fe627e321411c5ebdbbb7848f4fc87c60112a3
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-05-12 00:09:36 +0200

    Make VarId and Qvarid implement PsiNamedElement.
    Requirement #2 for Find Usages.

--- src/com/haskforce/psi/impl/HaskellPsiImplUtil.java
+++ src/com/haskforce/psi/impl/HaskellPsiImplUtil.java
@@ -1,7 +1,34 @@
 package com.haskforce.psi.impl;
 
-import com.intellij.lang.ASTNode;
-import com.haskforce.psi.HaskellTypes;
+import com.haskforce.psi.HaskellQvarid;
+import com.haskforce.psi.HaskellVarid;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * Source of the methods pointed out in Haskell.bnf.
+ */
 public class HaskellPsiImplUtil {
+
+    @NotNull
+    public static String getName(@NotNull HaskellVarid o) {
+        return o.getVaridRegexp().getText();
+    }
+
+    @NotNull
+    public static String getName(@NotNull HaskellQvarid o) {
+        return o.getVarid().getName();
+    }
+
+    @NotNull
+    public static PsiElement setName(@NotNull HaskellVarid o, @NotNull String newName) {
+        o.replace(HaskellElementFactory.createVaridFromText(o.getProject(), newName));
+        return o;
+    }
+
+    @NotNull
+    public static PsiElement setName(@NotNull HaskellQvarid o, @NotNull String newName) {
+        o.replace(HaskellElementFactory.createQvaridFromText(o.getProject(), newName));
+        return o;
+    }
 }
