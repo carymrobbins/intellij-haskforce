2f255c98adad1a4bf5a3248184ea624f1c45dd50
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-05-22 00:34:25 +0200

    Set "dist" to the compiler output directory.

--- src/com/haskforce/HaskellModuleBuilder.java
+++ src/com/haskforce/HaskellModuleBuilder.java
@@ -2,13 +2,18 @@ package com.haskforce;
 
 import com.intellij.ide.util.projectWizard.JavaModuleBuilder;
 import com.intellij.ide.util.projectWizard.ModuleBuilderListener;
+import com.intellij.ide.util.projectWizard.ModuleWizardStep;
+import com.intellij.ide.util.projectWizard.SettingsStep;
 import com.intellij.ide.util.projectWizard.SourcePathsBuilder;
+import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.projectRoots.SdkTypeId;
 import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class HaskellModuleBuilder extends JavaModuleBuilder implements SourcePathsBuilder, ModuleBuilderListener {
     @Override
@@ -17,18 +22,44 @@ public class HaskellModuleBuilder extends JavaModuleBuilder implements SourcePat
         super.setupRootModel(rootModel);
     }
 
+    /**
+     * Returns the Haskell module type.
+     */
     @Override
     public ModuleType getModuleType() {
         return HaskellModuleType.getInstance();
     }
 
+    /**
+     * Ensures that SDK type is a Haskell SDK.
+     */
     @Override
     public boolean isSuitableSdkType(SdkTypeId sdkType) {
         return sdkType == HaskellSdkType.getInstance();
     }
 
+    /**
+     * Called after module is created.
+     */
     @Override
     public void moduleCreated(@NotNull Module module) {
         // TODO
+    }
+
+    /**
+     * Hook into the new project creation and set dist to the compiler output
+     * directory.
+     */
+    @Override
+    @Nullable
+    public ModuleWizardStep modifySettingsStep(@NotNull SettingsStep settingsStep) {
+        if (settingsStep.getContext().isCreatingNewProject() &&
+                settingsStep.getContext().isProjectFileDirectorySet()) {
+            WizardContext c = settingsStep.getContext();
+            String path = c.getProjectFileDirectory();
+            String out = StringUtil.endsWithChar(path, '/') ? path + "dist" : path + "/dist";
+            c.setCompilerOutputDirectory(out);
+        }
+        return super.modifySettingsStep(settingsStep);
     }
 }
