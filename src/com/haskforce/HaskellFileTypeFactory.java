4cd504ee3cba28e7d286c3f0a3c260ff45186208
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-05-25 17:48:02 +0200

    Claim lhs files are covered by the plugin.

--- src/com/haskforce/HaskellFileTypeFactory.java
+++ src/com/haskforce/HaskellFileTypeFactory.java
@@ -7,6 +7,6 @@ import org.jetbrains.annotations.NotNull;
 public class HaskellFileTypeFactory extends FileTypeFactory {
     @Override
     public void createFileTypes(@NotNull FileTypeConsumer fileTypeConsumer) {
-        fileTypeConsumer.consume(HaskellFileType.INSTANCE, "hs");
+        fileTypeConsumer.consume(HaskellFileType.INSTANCE, "hs;lhs");
     }
 }
