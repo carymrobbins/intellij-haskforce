d978dbdca313eb8644bf513d43a9de3a1d540bf4
carymrobbins <carymrobbins@gmail.com>
2014-06-17 22:45:25 -0500

    Updated run config to save and reload configuration variables.

--- src/com/haskforce/run/HaskellRunConfiguration.java
+++ src/com/haskforce/run/HaskellRunConfiguration.java
@@ -5,8 +5,13 @@ import com.intellij.execution.ExecutionTarget;
 import com.intellij.execution.Executor;
 import com.intellij.execution.configurations.*;
 import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.openapi.components.PathMacroManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.InvalidDataException;
+import com.intellij.openapi.util.JDOMExternalizerUtil;
+import com.intellij.openapi.util.WriteExternalException;
+import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -16,6 +21,11 @@ import java.util.Collection;
  * Manages the data and execution of run configurations - Run->Edit Configurations->[+]->Haskell
  */
 public class HaskellRunConfiguration extends RunConfigurationBase {
+
+    // Element keys for readExternal and writeExternal to save configuration.
+    public static final String PROGRAM_ARGUMENTS = "PROGRAM_ARGUMENTS";
+
+    // Local configuration variables.
     public String programArguments;
 
     protected HaskellRunConfiguration(@NotNull Project project, @NotNull ConfigurationFactory factory, @NotNull String name) {
@@ -37,5 +47,19 @@ public class HaskellRunConfiguration extends RunConfigurationBase {
     @Override
     public RunProfileState getState(@NotNull Executor executor, @NotNull ExecutionEnvironment executionEnvironment) throws ExecutionException {
         return new HaskellCommandLineState(executionEnvironment, this);
+    }
+
+    @Override
+    public void readExternal(Element element) throws InvalidDataException {
+        PathMacroManager.getInstance(getProject()).expandPaths(element);
+        super.readExternal(element);
+        programArguments = JDOMExternalizerUtil.readField(element, PROGRAM_ARGUMENTS);
+    }
+
+    @Override
+    public void writeExternal(Element element) throws WriteExternalException {
+        super.writeExternal(element);
+        JDOMExternalizerUtil.writeField(element, PROGRAM_ARGUMENTS, programArguments);
+        PathMacroManager.getInstance(getProject()).collapsePathsRecursively(element);
     }
 }
