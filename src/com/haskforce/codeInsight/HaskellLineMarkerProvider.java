aa0f54224d34a4f57e3e4cba412005f642dcbec1
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-07-12 15:53:40 +0200

    Filter out non-definitions in the line marker targets.

--- src/com/haskforce/codeInsight/HaskellLineMarkerProvider.java
+++ src/com/haskforce/codeInsight/HaskellLineMarkerProvider.java
@@ -1,6 +1,7 @@
 package com.haskforce.codeInsight;
 
 import com.haskforce.HaskellIcons;
+import com.haskforce.psi.HaskellTypes;
 import com.haskforce.utils.HaskellUtil;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
@@ -8,6 +9,7 @@ import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNamedElement;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
@@ -26,8 +28,17 @@ public class HaskellLineMarkerProvider extends RelatedItemLineMarkerProvider {
             String value = namedElement.getName();
             if (value != null) {
                 Project project = element.getProject();
-                final List<PsiNamedElement> namedNodes =
+                final List<PsiNamedElement> namedNodes = ContainerUtil.newArrayList();
+                final List<PsiNamedElement> namedNodes2 =
                         HaskellUtil.findNamedNode(project, value);
+                // Find the nodes that are definitions.
+                for (PsiNamedElement e : namedNodes2) {
+                    if (e.getParent().getNode().getElementType().equals(HaskellTypes.VARS) &&
+                            !namedNodes.contains(e)) {
+                        namedNodes.add(e);
+                    }
+                }
+
                 if (namedNodes.size() > 0) {
                     NavigationGutterIconBuilder<PsiElement> builder =
                             NavigationGutterIconBuilder.create(HaskellIcons.FILE).
