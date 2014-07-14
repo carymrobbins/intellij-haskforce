4add1f3923a7b1e2c6096253cd48ceed21c46b81
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-07-14 16:49:20 +0200

    Add quickfix for renaming files to match module names.

--- src/com/haskforce/highlighting/HaskellAnnotator.java
+++ src/com/haskforce/highlighting/HaskellAnnotator.java
@@ -1,16 +1,22 @@
 package com.haskforce.highlighting;
 
-import com.haskforce.highlighting.HaskellSyntaxHighlighter;
 import com.haskforce.psi.*;
+import com.haskforce.quickfixes.HaskellModuleFilenameFix;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
-import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Annotator that:
+ *
+ * 1) brushes up syntax highlighting issues from parsing a broken program.
+ * 2) Registers quickfixes on broken nodes.
+ */
 public class HaskellAnnotator implements Annotator {
+    private static final String MSG = "File and module name differs";
     @Override
     public void annotate(@NotNull final PsiElement element, @NotNull final AnnotationHolder holder) {
         element.accept(new HaskellVisitor() {
@@ -54,6 +60,20 @@ public class HaskellAnnotator implements Annotator {
             public void visitReservedop(@NotNull HaskellReservedop o) {
                 super.visitReservedop(o);
                 setHighlighting(o, holder, HaskellSyntaxHighlighter.RESERVEDOP);
+            }
+
+            @Override
+            public void visitModuledecl(@NotNull HaskellModuledecl o) {
+                super.visitModuledecl(o);
+                HaskellQconid qc = o.getQconid();
+                String moduleName = qc.getConidRegexp().getText();
+                String fullFileName = o.getContainingFile().getName();
+                String fileSuffix = fullFileName.substring(fullFileName.lastIndexOf('.'));
+                String fileName = fullFileName.substring(0, fullFileName.length() - fileSuffix.length());
+                if (!moduleName.equals(fileName)) {
+                    HaskellModuleFilenameFix fix = new HaskellModuleFilenameFix(moduleName + fileSuffix);
+                    holder.createErrorAnnotation(qc, MSG).registerFix(fix);
+                }
             }
         });
     }
---
+++ src/com/haskforce/quickfixes/HaskellModuleFilenameFix.java
@@ -0,0 +1,92 @@
+package com.haskforce.quickfixes;
+
+import com.intellij.codeInsight.CodeInsightBundle;
+import com.intellij.codeInsight.intention.impl.BaseIntentionAction;
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.application.Result;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.ex.MessagesEx;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+/**
+ * Renames a file to match the module name.
+ */
+public class HaskellModuleFilenameFix extends BaseIntentionAction implements LocalQuickFix {
+    private final String myTargetName;
+
+    public HaskellModuleFilenameFix(@NotNull String newName) {
+        myTargetName = newName;
+    }
+
+    @Override
+    @NotNull
+    public String getText() {
+        return CodeInsightBundle.message("rename.file.fix");
+    }
+
+    @Override
+    @NotNull
+    public String getName() {
+        return getText();
+    }
+
+    @NotNull
+    @Override
+    public String getFamilyName() {
+        return "Haskell";
+    }
+
+    /**
+     * Checks whether this intention is available at a caret offset in file.
+     */
+    @Override
+    public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file) {
+        return true;
+    }
+
+    /**
+     * Called when user invokes intention.
+     */
+    @Override
+    public void invoke(@NotNull Project project, Editor editor, PsiFile file) throws IncorrectOperationException {
+        VirtualFile vFile = file.getVirtualFile();
+        Document document = PsiDocumentManager.getInstance(project).getDocument(file);
+        if (document == null) return;
+
+        FileDocumentManager.getInstance().saveDocument(document);
+        try {
+            vFile.rename(file.getManager(), myTargetName);
+        } catch (IOException e) {
+            MessagesEx.error(project, e.getMessage()).showLater();
+        }
+    }
+
+    @Override
+    public boolean startInWriteAction() {
+        return true;
+    }
+
+    @Override
+    public void applyFix(@NotNull final Project project, @NotNull ProblemDescriptor descriptor) {
+        final PsiFile file = descriptor.getPsiElement().getContainingFile();
+        if (isAvailable(project, null, file)) {
+            new WriteCommandAction(project) {
+                @Override
+                protected void run(Result result) throws Throwable {
+                    invoke(project, null, file);
+                }
+            }.execute();
+        }
+    }
+}
