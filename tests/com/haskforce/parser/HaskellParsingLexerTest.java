9c971a58bbbf744756a72ac6b59512527e737f60
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-07-21 14:28:52 +0200

    Add Import00005 to parsing lexer test suite.

--- tests/com/haskforce/parser/HaskellParsingLexerTest.java
+++ tests/com/haskforce/parser/HaskellParsingLexerTest.java
@@ -48,6 +48,7 @@ public class HaskellParsingLexerTest extends HaskellLexerTestBase {
     public void testImport00002()       { doTest(true, true); }
     public void testImport00003()       { doTest(true, true); }
     public void testImport00004()       { doTest(true, true); }
+    public void testImport00005()       { doTest(true, true); }
     public void testKind00001()         { doTest(true, true); }
     public void testKind00002()         { doTest(true, true); }
     public void testKind00003()         { doTest(true, true); }
