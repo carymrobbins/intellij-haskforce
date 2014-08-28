e9a5af59bf4a8230afab8875787bace063ff7d2c
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-08-28 19:14:38 +0200

    Shrink Layout 21 test.

--- tests/gold/parser/Layout00021.hs
+++ tests/gold/parser/Layout00021.hs
@@ -1,24 +1,7 @@
 module Layout00021 where
 
-runGhcModT :: IOish m
-           => Options
-           -> GhcModT m a
-           -> m (Either GhcModError a, GhcModLog)
 runGhcModT opt action = do
-    env <- liftBase $ newGhcModEnv opt =<< getCurrentDirectory
-    first (fst <$>) <$> (runGhcModT' env defaultState $ do
-        dflags <- getSessionDynFlags
-        defaultCleanupHandler dflags $ do
-            initializeFlagsWithCradle opt (gmCradle env)
-            action)
+    (f $ do action)
 
-runGhcModT' :: IOish m
-           => GhcModEnv
-           -> GhcModState
-           -> GhcModT m a
-           -> m (Either GhcModError (a, GhcModState), GhcModLog)
-runGhcModT' r s a = do
-  (res, w') <-
-      flip runReaderT r $ runJournalT $ runErrorT $
-        runStateT (unGhcModT $ initGhcMonad (Just ghcLibDir) >> a) s
-  return (res, w')
+runGhcModT' a = do
+  return res
