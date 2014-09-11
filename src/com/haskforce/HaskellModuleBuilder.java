7fc2de720945b9e836d64e19ae8ec626d174ac53
Cary Robbins <carymrobbins@gmail.com>
2014-09-10 21:48:49 -0500

    Update ignored files on module creation. closes #57

--- src/com/haskforce/HaskellModuleBuilder.java
+++ src/com/haskforce/HaskellModuleBuilder.java
@@ -6,6 +6,7 @@ import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 import com.intellij.ide.util.projectWizard.SettingsStep;
 import com.intellij.ide.util.projectWizard.SourcePathsBuilder;
 import com.intellij.ide.util.projectWizard.WizardContext;
+import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.options.ConfigurationException;
@@ -43,7 +44,21 @@ public class HaskellModuleBuilder extends JavaModuleBuilder implements SourcePat
      */
     @Override
     public void moduleCreated(@NotNull Module module) {
-        // TODO
+        // TODO - We should probably do some project initialization here as well...
+
+        // Update the ignored files and folders to avoid file search showing compiled files.
+        FileTypeManager fileTypeManager = FileTypeManager.getInstance();
+        StringBuilder builder = new StringBuilder(fileTypeManager.getIgnoredFilesList());
+        // Ensure the ignored file list ends with a semicolon.
+        if (builder.charAt(builder.length() - 1) != ';') {
+            builder.append(';');
+        }
+        for (String ignore : new String[]{"*.dyn_hi", "*.dyn_hi", "*.dyn_o", "*.hi", "*.o"}) {
+            if (builder.indexOf(';' + ignore + ';') == -1) {
+                builder.append(ignore).append(';');
+            }
+        }
+        fileTypeManager.setIgnoredFilesList(builder.toString());
     }
 
     /**
