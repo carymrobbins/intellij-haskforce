f09ea864d7271a9f26b5a80fd4577dea7c5e5d81
Cary Robbins <carymrobbins@gmail.com>
2015-01-09 00:35:56 -0600

    Account for leading slash escape in quasiquotes for language injections.

--- src/com/haskforce/psi/impl/HaskellQqblobManipulator.java
+++ src/com/haskforce/psi/impl/HaskellQqblobManipulator.java
@@ -1,10 +1,13 @@
 package com.haskforce.psi.impl;
 
 import com.haskforce.psi.HaskellQqblob;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.AbstractElementManipulator;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Pattern;
 
 public class HaskellQqblobManipulator extends AbstractElementManipulator<HaskellQqblob> {
     @Override
@@ -12,5 +15,24 @@ public class HaskellQqblobManipulator extends AbstractElementManipulator<Haskell
         final String oldText = psi.getText();
         final String newText = oldText.substring(0, range.getStartOffset()) + newContent + oldText.substring(range.getEndOffset());
         return psi.updateText(newText);
+    }
+
+    public static final Pattern LEADING_SLASH_ESCAPE_REGEX = Pattern.compile("\\\\\\s.*");
+
+    @NotNull
+    public TextRange getRangeInElement(@NotNull HaskellQqblob psi) {
+        return pairToTextRange(getRangeForText(psi.getText()));
+    }
+
+    private static Pair<Integer, Integer> getRangeForText(String text) {
+        final int start = LEADING_SLASH_ESCAPE_REGEX.matcher(text).matches() ? 2 : 1;
+        final int end = text.length() - start;
+        return new Pair(start, end);
+    }
+
+    private static TextRange pairToTextRange(Pair<Integer, Integer> pair) {
+        final int start = Math.max(pair.first, 0);
+        final int end = Math.max(pair.second, start);
+        return TextRange.from(start, end);
     }
 }
