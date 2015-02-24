0be459ded49949d099ed0780b6cc232a9f07607b
Kasper <kasper.janssens.android@gmail.com>
2015-02-24 21:21:56 +0100

    Cast to dependency or get text or so. Stuff seems to work but test is wrong

--- tests/com/haskforce/features/intentions/AddBuildDependsTest.java
+++ tests/com/haskforce/features/intentions/AddBuildDependsTest.java
@@ -3,8 +3,12 @@ package com.haskforce.features.intentions;
 import com.haskforce.HaskellLightPlatformCodeInsightFixtureTestCase;
 import com.haskforce.cabal.psi.CabalDependency;
 import com.haskforce.cabal.psi.CabalFile;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 
 //Maybe rename the haskelllightplatformcodeinsightfixturetestclass, as it actually has no
@@ -15,12 +19,17 @@ public class AddBuildDependsTest extends HaskellLightPlatformCodeInsightFixtureT
     }
 
     public void testInvoke() throws Exception {
-        myFixture.configureByFiles("addbuilddepends.cabal");
+        PsiFile[] psiFiles = myFixture.configureByFiles("addbuilddepends.cabal");
+        CabalFile cabalFile = (CabalFile)psiFiles[0];
         AddBuildDepends dingske = new AddBuildDepends("dingske");
         dingske.invoke(myFixture.getProject(),myFixture.getEditor(),null);
-        CabalFile cabalFile = (CabalFile)myFixture.getFile();
-        CabalDependency[] childrenOfType = PsiTreeUtil.getChildrenOfType(cabalFile, CabalDependency.class);
-        assertEquals(2, childrenOfType.length);
+        PsiElement firstChild = cabalFile.getFirstChild();
+        Collection<CabalDependency> cabalDependencies = PsiTreeUtil.findChildrenOfType(cabalFile, CabalDependency.class);
+        assertEquals(2, cabalDependencies.size());
+        for (CabalDependency cabalDependency : cabalDependencies) {
+            assertEquals(cabalDependency.getText(), "dingske");
+
+        }
 
     }
 }
