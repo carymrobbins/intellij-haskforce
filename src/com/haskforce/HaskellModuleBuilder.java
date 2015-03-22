26b7a10495499849997c378070d11b0f1dba09e7
Cary Robbins <carymrobbins@gmail.com>
2015-03-22 12:47:14 -0500

    Updated new project wizard to prompt for Haskell SDK; refs #141

--- src/com/haskforce/HaskellModuleBuilder.java
+++ src/com/haskforce/HaskellModuleBuilder.java
@@ -1,21 +1,18 @@
 package com.haskforce;
 
-import com.intellij.ide.util.projectWizard.JavaModuleBuilder;
-import com.intellij.ide.util.projectWizard.ModuleBuilderListener;
-import com.intellij.ide.util.projectWizard.ModuleWizardStep;
-import com.intellij.ide.util.projectWizard.SettingsStep;
-import com.intellij.ide.util.projectWizard.SourcePathsBuilder;
-import com.intellij.ide.util.projectWizard.WizardContext;
+import com.intellij.ide.util.projectWizard.*;
 import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.projectRoots.SdkTypeId;
 import com.intellij.openapi.roots.ModifiableRootModel;
-import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.util.Condition;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
 
 public class HaskellModuleBuilder extends JavaModuleBuilder implements SourcePathsBuilder, ModuleBuilderListener {
     @Override
@@ -62,21 +59,21 @@ public class HaskellModuleBuilder extends JavaModuleBuilder implements SourcePat
         fileTypeManager.setIgnoredFilesList(builder.toString());
     }
 
-    /**
-     * Hook into the new project creation and set dist to the compiler output
-     * directory.
-     */
     @Override
     @Nullable
     public ModuleWizardStep modifySettingsStep(@NotNull SettingsStep settingsStep) {
-        if (settingsStep.getContext().isCreatingNewProject() &&
-                settingsStep.getContext().isProjectFileDirectorySet()) {
-            WizardContext c = settingsStep.getContext();
-            String path = c.getProjectFileDirectory();
-            String out = StringUtil.endsWithChar(path, '/') ? path + "dist" : path + "/dist";
-            c.setCompilerOutputDirectory(out);
+        // Hook into the new project creation and set dist to the compiler output directory.
+        final WizardContext c = settingsStep.getContext();
+        if (c.isCreatingNewProject() && c.isProjectFileDirectorySet()) {
+            c.setCompilerOutputDirectory(new File(c.getProjectFileDirectory(), "dist").getPath());
         }
-        return super.modifySettingsStep(settingsStep);
+        // Prompt the user to set the Haskell SDK for the project.
+        return ProjectWizardStepFactory.getInstance().createJavaSettingsStep(settingsStep, this, new Condition<SdkTypeId>() {
+            @Override
+            public boolean value(SdkTypeId sdkType) {
+                return isSuitableSdkType(sdkType);
+            }
+        });
     }
 
     @Override
