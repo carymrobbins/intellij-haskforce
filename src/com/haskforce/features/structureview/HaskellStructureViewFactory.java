10dc197a65fd21b421b0b47e434e75a1703ee6e6
Cary M. Robbins <carymrobbins@gmail.com>
2015-04-10 23:16:33 -0500

    Updated build for IntelliJ 14.1.1

--- src/com/haskforce/features/structureview/HaskellStructureViewFactory.java
+++ src/com/haskforce/features/structureview/HaskellStructureViewFactory.java
@@ -4,6 +4,7 @@ import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -18,7 +19,7 @@ public class HaskellStructureViewFactory implements PsiStructureViewFactory {
         return new TreeBasedStructureViewBuilder() {
             @NotNull
             @Override
-            public StructureViewModel createStructureViewModel() {
+            public StructureViewModel createStructureViewModel(@Nullable Editor editor) {
                 return new HaskellStructureViewModel(psiFile);
             }
         };
