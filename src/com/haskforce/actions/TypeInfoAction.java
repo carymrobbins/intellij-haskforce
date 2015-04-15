2069c11f155502397181adddddba7883aba63476
Cary M. Robbins <carymrobbins@gmail.com>
2015-04-15 13:10:45 -0500

    Updated type info action to only run on Haskell files; fixes #156

--- src/com/haskforce/actions/TypeInfoAction.java
+++ src/com/haskforce/actions/TypeInfoAction.java
@@ -1,13 +1,16 @@
 package com.haskforce.actions;
 
 import com.haskforce.highlighting.annotation.external.TypeInfoUtil;
+import com.haskforce.psi.HaskellFile;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.psi.PsiFile;
 import com.intellij.ui.awt.RelativePoint;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,6 +20,15 @@ import org.jetbrains.annotations.NotNull;
  * containing the types found
  */
 public class TypeInfoAction extends AnAction {
+    /**
+     * Only enable type info when in a Haskell file.
+     */
+    @Override
+    public void update(AnActionEvent e) {
+        final PsiFile file = e.getData(CommonDataKeys.PSI_FILE);
+        e.getPresentation().setEnabled(file != null && file instanceof HaskellFile);
+    }
+
     public void actionPerformed(@NotNull AnActionEvent e) {
         Project project = e.getProject();
         if (project == null){
