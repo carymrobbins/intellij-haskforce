e2baf0a778dfe22582b5018d0de396a644cefeb6
Cary M. Robbins <carymrobbins@gmail.com>
2015-05-11 15:23:22 -0500

    Created SComboBox to enable JComboBox with multiple JDKs.

---
+++ macros/src/com/haskforce/ui/SComboBoxMacro.scala
@@ -0,0 +1,24 @@
+package com.haskforce.ui
+
+import scala.annotation.StaticAnnotation
+import scala.language.experimental.macros
+import scala.reflect.macros.whitebox
+
+/**
+ * Macro annotation to rewrite the class definition for SComboBox.
+ * The problem is that JComboBox on JDK6 does not take type parameters, but the one for JDK7+ does.
+ * This causes problems when targeting multiple JDKs with Scala since type parameters are not optional.
+ */
+class SComboBoxMacro extends StaticAnnotation {
+  def macroTransform(annottees: Any*): Any = macro SComboBoxMacro.impl
+}
+
+object SComboBoxMacro {
+  def impl(c: whitebox.Context)(annottees: c.Expr[Any]*): c.universe.Tree = {
+    import c.universe._ // Needed for quasiquotes
+    Class.forName("javax.swing.JComboBox").getTypeParameters.length match {
+      case 0 => q"""class SComboBox[E] extends JComboBox"""
+      case 1 => q"""class SComboBox[E] extends JComboBox[E]"""
+    }
+  }
+}
---
+++ src/com/haskforce/ui/SComboBox.scala
@@ -0,0 +1,8 @@
+package com.haskforce.ui
+
+import javax.swing.JComboBox
+
+/**
+ * Scala-friendly JComboBox; see [[SComboBoxMacro]] for more details.
+ */
+@SComboBoxMacro class SComboBox[E] extends JComboBox[E]
