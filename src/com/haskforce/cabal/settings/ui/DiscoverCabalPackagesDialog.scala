bda94dd0b029045536464ef26bec6a1deb6a4013
Cary M. Robbins <carymrobbins@gmail.com>
2015-06-27 05:22:47 -0500

    Cleaned up the Discover Cabal Packages form layout.

--- src/com/haskforce/cabal/settings/ui/DiscoverCabalPackagesDialog.scala
+++ src/com/haskforce/cabal/settings/ui/DiscoverCabalPackagesDialog.scala
@@ -1,6 +1,6 @@
 package com.haskforce.cabal.settings.ui
 
-import java.awt.event.ActionEvent
+import java.awt.event.{KeyEvent, ActionEvent}
 import java.awt.{Dimension, GridBagLayout}
 import javax.swing._
 
@@ -9,6 +9,7 @@ import com.haskforce.ui.GC
 import com.haskforce.utils.FileUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.uiDesigner.core.Spacer
 
 /**
  * Dialog for importing Cabal packages into IntelliJ modules.
@@ -35,6 +36,7 @@ class DiscoverCabalPackagesDialog(
     setupListeners()
     pack()
     setModal(true)
+    setResizable(false)
     setVisible(true)
   }
 
@@ -47,6 +49,12 @@ class DiscoverCabalPackagesDialog(
     buttonCancel.addActionListener { e: ActionEvent => onCancel() }
     fieldMap.foreach { case (_, checkBox) => checkBox.addActionListener { e: ActionEvent => onCheck() } }
     checkBoxImportAll.addActionListener { e: ActionEvent => onCheckImportAll() }
+    // Cancel when user presses Escape.
+    contentPane.registerKeyboardAction(
+      { e: ActionEvent => onCancel() },
+      KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+      JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT
+    )
   }
 
   private def getCabalFilesToImport: Seq[VirtualFile] = fieldMap.collect {
@@ -84,23 +92,24 @@ class DiscoverCabalPackagesDialog(
   }
 
   private def setupComponents(): Unit = {
-    val gc = GC(weightX = 0.5, weightY = 0.5, height = 1, padX = 10, padY = 5)
+    val gc = GC.weight(x = 0.5).pad(10, 5).northWest
 
     val listPanel = new JPanel(new GridBagLayout) {
       add(checkBoxImportAll, gc.grid(0, 0))
-      add(new JLabel("<html><b>Package</b></html>"), gc.west.grid(1, 0))
-      add(new JLabel("<html><b>Directory</b></html>"), gc.west.grid(2, 0))
+      add(new JLabel("<html><b>Package</b></html>"), gc.grid(1, 0))
+      add(new JLabel("<html><b>Directory</b></html>"), gc.grid(2, 0))
       add(new JSeparator(SwingConstants.HORIZONTAL), gc.fillHorizontal.width(3).grid(0, 1))
 
       fieldMap.zipWithIndex.foreach { case ((file, shouldImport), i) =>
         val y = i + 2
         add(shouldImport, gc.grid(0, y))
-        add(new JLabel(file.getNameWithoutExtension), gc.west.grid(1, y))
-        add(new JLabel(getPackagePath(file)), gc.west.grid(2, y))
+        add(new JLabel(file.getNameWithoutExtension), gc.grid(1, y))
+        add(new JLabel(getPackagePath(file)), gc.grid(2, y))
       }
+      add(new Spacer, gc.weight(y = 1).grid(0, fieldMap.length + 2))
     }
     val scrollPane = new JScrollPane(listPanel)
-    scrollPane.setPreferredSize(new Dimension(800, 450))
+    scrollPane.setPreferredSize(new Dimension(800, getListPanelHeight))
     getContentPane.add(scrollPane, gc.fillBoth.grid(0, 0))
 
     val buttonPanel = new JPanel(new GridBagLayout) {
@@ -111,4 +120,6 @@ class DiscoverCabalPackagesDialog(
 
     validate()
   }
+
+  private def getListPanelHeight: Int = Math.min((fieldMap.length + 1) * 30, 450)
 }
