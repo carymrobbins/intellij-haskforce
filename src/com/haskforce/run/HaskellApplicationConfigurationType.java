0a46b3ae0a4c19383b2f773946f1d77fe02cc952
Cary M. Robbins <carymrobbins@gmail.com>
2015-11-04 00:55:40 -0600

    refs #167: Change names and icons of run configs to 'Cabal'

--- src/com/haskforce/run/HaskellApplicationConfigurationType.java
+++ src/com/haskforce/run/HaskellApplicationConfigurationType.java
@@ -1,6 +1,7 @@
 package com.haskforce.run;
 
 import com.haskforce.HaskellIcons;
+import com.haskforce.cabal.CabalIcons;
 import com.intellij.execution.configurations.ConfigurationTypeBase;
 import com.intellij.openapi.extensions.Extensions;
 import com.intellij.util.containers.ContainerUtil;
@@ -9,10 +10,10 @@ import com.intellij.util.containers.ContainerUtil;
  * The "Haskel" option under Run->Edit Configurations->[+]
  */
 public class HaskellApplicationConfigurationType extends ConfigurationTypeBase {
-    public static final String HASKELL_CONFIG_ID = "Haskell Application Configuration";
+    public static final String HASKELL_CONFIG_ID = "Cabal Run Configuration";
 
     protected HaskellApplicationConfigurationType() {
-        super(HASKELL_CONFIG_ID, "Haskell Application", "Run a Haskell application.", HaskellIcons.FILE);
+        super(HASKELL_CONFIG_ID, "Cabal Run", "Execute a `cabal run` task.", CabalIcons.FILE);
         addFactory(new HaskellApplicationConfigurationFactory(this));
     }
 
--- src/com/haskforce/run/HaskellTestConfigurationType.java
+++ src/com/haskforce/run/HaskellTestConfigurationType.java
@@ -1,6 +1,7 @@
 package com.haskforce.run;
 
 import com.haskforce.HaskellIcons;
+import com.haskforce.cabal.CabalIcons;
 import com.intellij.execution.configurations.ConfigurationTypeBase;
 import com.intellij.openapi.extensions.Extensions;
 import com.intellij.util.containers.ContainerUtil;
@@ -9,10 +10,10 @@ import com.intellij.util.containers.ContainerUtil;
  * The "Haskel" option under Run->Edit Configurations->[+]
  */
 public class HaskellTestConfigurationType extends ConfigurationTypeBase {
-    public static final String HASKELL_CONFIG_ID = "Haskell Test Configuration";
+    public static final String HASKELL_CONFIG_ID = "Cabal Test Configuration";
 
     protected HaskellTestConfigurationType() {
-        super(HASKELL_CONFIG_ID, "Haskell Test", "Run Haskell tests.", HaskellIcons.TEST_FILE);
+        super(HASKELL_CONFIG_ID, "Cabal Test", "Execute a `cabal test` task.", CabalIcons.FILE);
         addFactory(new HaskellTestConfigurationFactory(this));
     }
 
