297b38d17853865a11a8bbb52186c0fde09e8940
Cary M. Robbins <carymrobbins@gmail.com>
2015-11-15 18:32:18 -0500

    Updated type info action to use hint popup and disappear on keypress

--- src/com/haskforce/actions/TypeInfoAction.java
+++ src/com/haskforce/actions/TypeInfoAction.java
@@ -2,17 +2,25 @@ package com.haskforce.actions;
 
 import com.haskforce.highlighting.annotation.external.TypeInfoUtil;
 import com.haskforce.psi.HaskellFile;
+import com.intellij.codeInsight.hint.HintManager;
+import com.intellij.codeInsight.hint.HintManagerImpl;
+import com.intellij.codeInsight.hint.HintUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.LogicalPosition;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.popup.JBPopup;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.psi.PsiFile;
-import com.intellij.ui.awt.RelativePoint;
+import com.intellij.ui.LightweightHint;
+import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 /**
  * This action will show the type information on the position of the cursor or the selection.
@@ -36,14 +44,28 @@ public class TypeInfoAction extends AnAction {
         }
         FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
         String typeInfo = TypeInfoUtil.getTypeInfo(project);
-        JBPopupFactory jbPopupFactory = JBPopupFactory.getInstance();
-        Editor selectedTextEditor = fileEditorManager.getSelectedTextEditor();
-        if (selectedTextEditor == null){
+        if (typeInfo == null) {
+            // TODO: getTypeInfo should probably be @NotNull
             return;
         }
-        RelativePoint relativePoint = jbPopupFactory.guessBestPopupLocation(
-                selectedTextEditor);
-        JBPopup message = jbPopupFactory.createMessage(typeInfo);
-        message.show(relativePoint);
+        JComponent label = HintUtil.createInformationLabel(typeInfo);
+        label.setFont(UIUtil.getLabelFont());
+        final LightweightHint hint = new LightweightHint(label);
+        final HintManagerImpl manager = HintManagerImpl.getInstanceImpl();
+        label.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                manager.hideAllHints();
+            }
+        });
+        Editor editor = fileEditorManager.getSelectedTextEditor();
+        // TODO: Refactor this, editor shouldn't be null since it's checked in getTypeInfo
+        assert editor != null;
+        LogicalPosition position = editor.getCaretModel().getLogicalPosition();
+        Point point = HintManagerImpl.getHintPosition(hint, editor, position, HintManager.ABOVE);
+        int flags = HintManager.HIDE_BY_ANY_KEY |
+                HintManager.HIDE_BY_TEXT_CHANGE |
+                HintManager.HIDE_BY_SCROLLING;
+        manager.showEditorHint(hint, editor, point, flags, 0, false);
     }
 }
