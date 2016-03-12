23053298b01f3a1310d29021a5b562b1d059db86
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2016-03-12 18:07:27 +0100

    refs #238: create configurationType for Haskell Stack Run

---
+++ src/com/haskforce/run/stack/StackApplicationConfigurationType.scala
@@ -0,0 +1,14 @@
+package com.haskforce.run.stack
+
+import com.haskforce.HaskellIcons
+import com.intellij.execution.configurations.{RunConfiguration, ConfigurationFactory, ConfigurationTypeBase}
+import com.intellij.openapi.project.Project
+
+class StackApplicationConfigurationType extends ConfigurationTypeBase("Stack Run Configuration", "Haskell Stack Run", "Execute a `stack exec` task.", HaskellIcons.FILE) {
+
+  addFactory(new ConfigurationFactory(this){
+    override def createTemplateConfiguration(project: Project): RunConfiguration = {
+      return new StackApplicationRunConfiguration(project, this)
+    }
+  })
+}
---
+++ src/com/haskforce/run/stack/StackRunCommandLineState.scala
@@ -0,0 +1,24 @@
+package com.haskforce.run.stack
+
+import com.haskforce.settings.HaskellBuildSettings
+import com.intellij.execution.configurations.{ParametersList, GeneralCommandLine, CommandLineState}
+import com.intellij.execution.process.{OSProcessHandler}
+import com.intellij.execution.runners.ExecutionEnvironment
+
+class StackRunCommandLineState(environment: ExecutionEnvironment,
+                               config: StackApplicationRunConfiguration) extends CommandLineState(environment) {
+
+  // run `stack exec <executable> -- <executableArguments>`
+  override def startProcess(): OSProcessHandler = {
+    val commandLine: GeneralCommandLine = new GeneralCommandLine
+    commandLine.setWorkDirectory(getEnvironment.getProject.getBasePath)
+    val buildSettings: HaskellBuildSettings = HaskellBuildSettings.getInstance(config.getProject)
+    commandLine.setExePath(buildSettings.getStackPath)
+    val parametersList: ParametersList = commandLine.getParametersList
+    parametersList.add("exec")
+    parametersList.add(config.executable)
+    parametersList.add("--")
+    parametersList.addParametersString(config.executableArguments)
+    return new OSProcessHandler(commandLine)
+  }
+}
