b599c50db54a030df382b5c18a14697dce90f793
Cary M. Robbins <carymrobbins@gmail.com>
2016-08-01 14:10:19 -0500

    refs #278: Avoid Java 8 classes in unsupporting JREs

--- jps-plugin/src/com/haskforce/jps/HaskForceBuilder.java
+++ jps-plugin/src/com/haskforce/jps/HaskForceBuilder.java
@@ -1,6 +1,7 @@
 package com.haskforce.jps;
 
 import com.haskforce.jps.stack.StackBuilder;
+import com.haskforce.utils.JavaVersionUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.incremental.BuilderService;
 import org.jetbrains.jps.incremental.ModuleLevelBuilder;
@@ -17,6 +18,10 @@ public class HaskForceBuilder extends BuilderService {
     @NotNull
     @Override
     public List<? extends ModuleLevelBuilder> createModuleLevelBuilders() {
+        // See: https://github.com/carymrobbins/intellij-haskforce/issues/278
+        // When we are compiling a Haskell project, the JDK should be at least Java 8.
+        // Only Java projects will use something older.
+        if (!JavaVersionUtil.isAtLeastJava8()) return Collections.emptyList();
         return Collections.singletonList(new CabalBuilder());
     }
 
