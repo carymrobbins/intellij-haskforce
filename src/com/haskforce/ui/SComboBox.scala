32c47f9db0bd61e218674e6a4635261eaf9ded76
Cary M. Robbins <carymrobbins@gmail.com>
2016-08-16 23:45:46 -0500

    refs #292: Remove unnecessary UI macros
    These macros were only used when we still targeted Java 6 to get around
    limitations between swing classes which had type arguments as of Java 7.
    Now that we only target Java 8 this is a non-issue.

--- src/com/haskforce/ui/SComboBox.scala
+++ src/com/haskforce/ui/SComboBox.scala
@@ -2,11 +2,4 @@ package com.haskforce.ui
 
 import javax.swing.JComboBox
 
-import com.haskforce.macros.ui.SComboBoxMacro
-
-/**
- * Scala-friendly JComboBox; see [[SComboBoxMacro]] for more details.
- */
-@SComboBoxMacro class SComboBox[E] extends JComboBox[E] {
-
-}
+final class SComboBox[E] extends JComboBox[E]
--- src/com/haskforce/ui/SListCellRendererWrapper.scala
+++ src/com/haskforce/ui/SListCellRendererWrapper.scala
@@ -1,8 +1,13 @@
 package com.haskforce.ui
 
-import javax.swing.ListCellRenderer
+import javax.swing.JList
 
-import com.haskforce.macros.ui.SListCellRendererWrapperMacro
+import com.intellij.ui.ListCellRendererWrapper
 
-@SListCellRendererWrapperMacro
-class SListCellRendererWrapper[A](showItem: A => String) extends ListCellRenderer[A]
+/** Create a ListCellRenderer from a simple function A => String */
+class SListCellRendererWrapper[A](showItem: A => String) extends ListCellRendererWrapper[A] {
+  override def customize(jList: JList[_], t: A, i: Int, b: Boolean, b1: Boolean): Unit = {
+    if (t == null) return
+    setText(showItem(t))
+  }
+}
