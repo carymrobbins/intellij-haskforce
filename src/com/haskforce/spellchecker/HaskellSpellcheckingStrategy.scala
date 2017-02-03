8c02c78ffc38f059217bf268a0ed221747779335
Cary Robbins <carymrobbins@gmail.com>
2017-02-02 20:26:22 -0500

    refs #320: Don't report typos more than once

--- src/com/haskforce/spellchecker/HaskellSpellcheckingStrategy.scala
+++ src/com/haskforce/spellchecker/HaskellSpellcheckingStrategy.scala
@@ -3,16 +3,12 @@ package com.haskforce.spellchecker
 import com.haskforce.HaskellLanguage
 import com.haskforce.cabal.CabalLanguage
 import com.intellij.psi.PsiElement
-import com.intellij.spellchecker.tokenizer.{Tokenizer, SpellcheckingStrategy}
+import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy
 
 /**
  * Provide spellchecker support for Haskell/Cabal sources.
  */
 class HaskellSpellcheckingStrategy extends SpellcheckingStrategy {
-  // Use TEXT_TOKENIZER so prime' names won't be marked as a typo.
-  override def getTokenizer(element: PsiElement): Tokenizer[_ <: PsiElement] = {
-    SpellcheckingStrategy.TEXT_TOKENIZER
-  }
 
   override def isMyContext(element: PsiElement): Boolean = {
     Seq(
--- tests/com/haskforce/spellchecker/HaskellSpellcheckingTest.scala
+++ tests/com/haskforce/spellchecker/HaskellSpellcheckingTest.scala
@@ -3,6 +3,8 @@ package com.haskforce.spellchecker
 import com.haskforce.HaskellLightPlatformCodeInsightFixtureTestCase
 import com.intellij.spellchecker.inspections.SpellCheckingInspection
 
+import scala.collection.JavaConverters._
+
 /**
  * Tests for the Haskell spellchecker.
  */
@@ -16,6 +18,24 @@ class HaskellSpellcheckingTest extends HaskellLightPlatformCodeInsightFixtureTes
 
   private def doTest(): Unit = {
     myFixture.testHighlighting(false, false, true, getTestName(false) + ".hs")
+    assertUniqueElements()
+  }
+
+  /**
+   * Ensures that all typos found are unique by element. It seems that
+   * [[com.intellij.spellchecker.tokenizer.SpellcheckingStrategy.TEXT_TOKENIZER]]
+   * causes duplicate warnings for the same typo, so this assertion ensures that
+   * doesn't happen.
+   *
+   * Note that this must be called _after_ [[myFixture.testHighlighting]] which
+   * tells [[myFixture.doHighlighting]] which file we're asserting for.
+   *
+   */
+  private def assertUniqueElements(): Unit = {
+    val duplicates = myFixture.doHighlighting().asScala.groupBy(_.getText).collect {
+      case (k, v) if v.length > 1 => (k, v.length)
+    }
+    assert(duplicates.isEmpty, s"Duplicate warnings found: $duplicates")
   }
 
   def testSpelling() = doTest()
