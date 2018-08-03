1a2be51bdd8984eebb50c374df515e6be4c92172
Cary Robbins <carymrobbins@gmail.com>
2018-08-03 18:30:18 -0500

    Auto-import type constructor/class

--- src/com/haskforce/highlighting/annotation/external/GhcMod.java
+++ src/com/haskforce/highlighting/annotation/external/GhcMod.java
@@ -330,7 +330,14 @@ public class GhcMod {
                                 public void apply(Matcher matcher, Annotation annotation, Problem problem) {
                                     annotation.registerFix(new AddToImports(matcher.group(1)));
                                 }
-                            })
+                            }),
+              Pair.create(Pattern.compile("Not in scope: type constructor or class ‘([^’]+)’"),
+                new RegisterFixHandler() {
+                    @Override
+                    public void apply(Matcher matcher, Annotation annotation, Problem problem) {
+                        annotation.registerFix(new AddToImports(matcher.group(1)));
+                    }
+                })
             ));
         }
 
