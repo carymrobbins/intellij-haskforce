40114476b6da4a19d33855d528d01f50b61f155b
Cary Robbins <carymrobbins@gmail.com>
2018-11-17 13:05:57 -0600

    Attempt at parsing relative path links in 'stack task' output

--- src/com/haskforce/run/stack/task/StackTaskCommandLineState.scala
+++ src/com/haskforce/run/stack/task/StackTaskCommandLineState.scala
@@ -3,12 +3,14 @@ package com.haskforce.run.stack.task
 import java.util.regex.Pattern
 
 import com.haskforce.settings.HaskellBuildSettings
+import com.haskforce.utils.FileUtil
 import com.intellij.execution.configurations.{CommandLineState, GeneralCommandLine, ParametersList}
 import com.intellij.execution.filters._
 import com.intellij.execution.impl.ConsoleViewImpl
 import com.intellij.execution.process.OSProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.ui.ConsoleView
+import com.intellij.openapi.vfs.{LocalFileSystem, VirtualFileManager}
 
 class StackTaskCommandLineState(
   environment: ExecutionEnvironment,
@@ -23,10 +25,36 @@ class StackTaskCommandLineState(
         config.getProject.getBasePath,
         new PatternBasedFileHyperlinkRawDataFinder(Array(
           new PatternHyperlinkFormat(
-            Pattern.compile("^([^:]+):(\\d+):(\\d+):"), false, false,
+            Pattern.compile("^\\s*([^:]+):(\\d+):(\\d+):"), false, false,
             PatternHyperlinkPart.PATH, PatternHyperlinkPart.LINE, PatternHyperlinkPart.COLUMN
           )
-        ))
+        )) {
+          override def find(line: String): java.util.List[FileHyperlinkRawData] = {
+            val res = super.find(line)
+            if (res.isEmpty) return res
+            val fs = LocalFileSystem.getInstance()
+            // TODO: Probably should iter
+            val data = res.get(0)
+            if (fs.findFileByPath(data.getFilePath) != null) return res
+            // Infer relative path, would be nice if we could figure this out somehow from the command
+            // or output to determine base relative dir instead of just guessing like this.
+
+            val found = FileUtil.findFilesRecursively(
+              getProject.getBaseDir,
+              _.getCanonicalPath.endsWith(data.getFilePath)
+            )
+            // Abort if we found 0 or more than 1 match since that would be ambiguous.
+            if (found.length != 1) return res
+            // Hopefully we did this right
+            java.util.Collections.singletonList(new FileHyperlinkRawData(
+              found.head.getCanonicalPath,
+              data.getDocumentLine,
+              data.getDocumentColumn,
+              data.getHyperlinkStartInd,
+              data.getHyperlinkEndInd
+            ))
+          }
+        }
       ))
       consoleView
     }
