9a8b82bb6c96db10040c1982843a6bc5324c4418
Cary Robbins <carymrobbins@gmail.com>
2019-05-27 11:23:48 -0500

    refs #392: Parse NamedFieldPuns

--- tests/com/haskforce/parser/HaskellParsingLexerTest.java
+++ tests/com/haskforce/parser/HaskellParsingLexerTest.java
@@ -30,6 +30,9 @@ public class HaskellParsingLexerTest extends HaskellLexerTestBase {
     public void testCase00001()         { doTest(true, true); }
     public void testDerivingStrategies00001() { doTest(true, true); }
     public void testDerivingVia00001()  { doTest(true, true); }
+    public void testRecord00001()       { doTest(true, true); }
+    public void testRecord00002()       { doTest(true, true); }
+    public void testRecord00003()       { doTest(true, true); }
     public void testEta00001()          { doTest(true, true); }
     public void testFun00001()          { doTest(true, true); }
     public void testFun00002()          { doTest(true, true); }
