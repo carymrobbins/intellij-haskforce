3819ede1ba71d65287f1bb30dab831dc1b99c721
Cary Robbins <carymrobbins@gmail.com>
2019-12-02 12:12:37 -0600

    refs #381: Added hsdev to CompileProblemsProviderFactory

--- src/com/haskforce/highlighting/annotation/external/CompileProblemsProviderFactory.scala
+++ src/com/haskforce/highlighting/annotation/external/CompileProblemsProviderFactory.scala
@@ -1,12 +1,12 @@
 package com.haskforce.highlighting.annotation.external
 
 import com.intellij.psi.PsiFile
-
-import com.haskforce.highlighting.annotation.external.impl.{GhcModiCompileProblemsProvider, GhcModCompileProblemsProvider}
+import com.haskforce.highlighting.annotation.external.impl.{GhcModCompileProblemsProvider, GhcModiCompileProblemsProvider, HsDevCompileProblemsProvider}
 
 object CompileProblemsProviderFactory extends ProblemsProviderFactory {
   def get(psiFile: PsiFile): Option[ProblemsProvider] = {
-    GhcModiCompileProblemsProvider.create(psiFile)
+    HsDevCompileProblemsProvider.create(psiFile)
+      .orElse(GhcModiCompileProblemsProvider.create(psiFile))
       .orElse(GhcModCompileProblemsProvider.create(psiFile))
   }
 }
