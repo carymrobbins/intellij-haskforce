009ef8498831ba8f390d21542c93133829c9f04d
Cary Robbins <carymrobbins@gmail.com>
2020-01-24 19:17:49 -0600

    Avoid NPE in assertion failure in HaskellResolveTestCase

--- tests/com/haskforce/resolve/HaskellResolveTestCase.java
+++ tests/com/haskforce/resolve/HaskellResolveTestCase.java
@@ -1,17 +1,19 @@
 package com.haskforce.resolve;
 
 import com.haskforce.HaskellLightPlatformCodeInsightFixtureTestCase;
+import com.haskforce.psi.HaskellFile;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.CharsetToolkit;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiReference;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.util.Collection;
-import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertNotEquals;
 
 public abstract class HaskellResolveTestCase extends HaskellLightPlatformCodeInsightFixtureTestCase {
     protected PsiReference referencedElement;
@@ -45,11 +47,14 @@ public abstract class HaskellResolveTestCase extends HaskellLightPlatformCodeIns
             text = text.replace("<ref>", "");
             int resolvedOffset = text.indexOf("<resolved>");
             text = text.replace("<resolved>", "");
+            if (referencedOffset == -1 && resolvedOffset == -1) continue;
             String relativePath = file.getCanonicalPath().substring(
               file.getCanonicalPath().indexOf(getTestDataPath()) + getTestDataPath().length() + 1
             );
-            VirtualFile vFile = myFixture.getTempDirFixture().createFile(relativePath, text);
+            myFixture.getTempDirFixture().createFile(relativePath, text);
             PsiFile psiFile = myFixture.configureFromTempProjectFile(relativePath);
+//            PsiFile psiFile = myFixture.configureByText(relativePath, text);
+            assertInstanceOf(psiFile, HaskellFile.class);
             if (referencedOffset != -1) {
                 referencedElement = psiFile.findReferenceAt(referencedOffset);
                 if (referencedElement == null) fail("Reference was null in " + file.getName());
@@ -58,7 +63,6 @@ public abstract class HaskellResolveTestCase extends HaskellLightPlatformCodeIns
                 final PsiReference ref = psiFile.findReferenceAt(resolvedOffset);
                 if (ref == null) { fail("Reference was null in " + file.getName()); }
                 resolvedElement = ref.getElement();
-                if (resolvedElement == null) { fail("Reference returned null element in " + file.getName()); }
             }
         }
     }
@@ -69,16 +73,32 @@ public abstract class HaskellResolveTestCase extends HaskellLightPlatformCodeIns
         if (succeed && referencedElement == null) { fail("Could not find reference at caret."); }
         if (succeed && resolvedElement == null) { fail("Could not find resolved element."); }
         if (succeed) {
-            PsiElement resolvedActual = referencedElement.resolve();
             assertEquals(
-              "Could not resolve expected reference.\n" +
-                "Expected: " + resolvedElement + " (" + resolvedElement.getText() + ")\n" +
-                "Actual: " + resolvedActual + " (" + resolvedActual.getText() + ")",
-              resolvedElement,
-              resolvedActual
+              "Failed to resolve reference",
+              new PrettyPsiElementWrapper(resolvedElement),
+              new PrettyPsiElementWrapper(referencedElement.resolve())
             );
         } else {
-            assertFalse("Resolved unexpected reference.", resolvedElement.equals(referencedElement.resolve()));
+            assertNotEquals(
+              "Resolved unexpected reference",
+              new PrettyPsiElementWrapper(resolvedElement),
+              new PrettyPsiElementWrapper(referencedElement.resolve())
+            );
+        }
+    }
+
+    /** Hack to improve assertion messages comparing elements. */
+    private static class PrettyPsiElementWrapper {
+        private final @Nullable PsiElement element;
+
+        PrettyPsiElementWrapper(@Nullable PsiElement element) {
+            this.element = element;
+        }
+
+        @Override
+        public String toString() {
+            if (element == null) return "null";
+            return element.toString() + " [text = \"" + element.getText() + "\"]";
         }
     }
 }
