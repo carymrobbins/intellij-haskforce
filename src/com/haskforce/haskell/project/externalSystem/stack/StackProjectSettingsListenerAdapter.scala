ecf46cf8259174450369f4143302e07cb557bc46
Cary Robbins <carymrobbins@gmail.com>
2020-06-15 17:51:13 -0500

    [stack-manager] Fix minor compile issues

--- src/com/haskforce/haskell/project/externalSystem/stack/StackProjectSettingsListenerAdapter.scala
+++ src/com/haskforce/haskell/project/externalSystem/stack/StackProjectSettingsListenerAdapter.scala
@@ -1,11 +1,13 @@
 package com.haskforce.haskell.project.externalSystem.stack
 
 import com.intellij.openapi.externalSystem.service.project.manage.ExternalProjectsManager
+import com.intellij.openapi.externalSystem.settings.ExternalSystemSettingsListenerAdapter
 import com.intellij.openapi.project.Project
 
 class StackProjectSettingsListenerAdapter(
   project: Project
-) extends StackProjectSettingsListener {
+) extends ExternalSystemSettingsListenerAdapter[StackProjectSettings]
+  with StackProjectSettingsListener {
 
   def onStackProjectSettingsChange(): Unit = {
     ExternalProjectsManager.getInstance(project)
