6e18b84d798084fc6936b3eb31854348916d8e27
Cary Robbins <carymrobbins@gmail.com>
2020-06-24 17:36:46 -0500

    [stack-manager] Simplify re-import detection

--- src/com/haskforce/haskell/project/externalSystem/stack/StackAutoImportAware.scala
+++ src/com/haskforce/haskell/project/externalSystem/stack/StackAutoImportAware.scala
@@ -4,7 +4,9 @@ import java.io.File
 import java.nio.file.{Files, Paths}
 import java.util
 
+import com.haskforce.importWizard.stack.StackYaml
 import com.haskforce.settings.HaskellBuildSettings
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.externalSystem.ExternalSystemAutoImportAware
 import com.intellij.openapi.project.Project
 
@@ -13,6 +15,8 @@ import com.intellij.openapi.project.Project
   * Mostly adapted from GradleAutoImportAware
   */
 object StackAutoImportAware extends ExternalSystemAutoImportAware {
+
+  private val LOG = Logger.getInstance(getClass)
 
   override def getAffectedExternalProjectPath(
     changedFileOrDirPath: String,
@@ -44,20 +48,43 @@ object StackAutoImportAware extends ExternalSystemAutoImportAware {
     projectPath: String,
     project: Project
   ): util.List[File] = {
-    val files = new util.ArrayList[File]()
-    val configuredStackFile = HaskellBuildSettings.getInstance(project).getStackFile
-    if (configuredStackFile != null) {
-      files.add(new File(configuredStackFile))
+    val stackYamlPath =
+      Option(HaskellBuildSettings.getInstance(project).getStackFile)
+        .map(Paths.get(_))
+        .filter(Files.exists(_))
+        .getOrElse(Paths.get(projectPath, "stack.yaml"))
+    // If we couldn't find a stack.yaml, give up.
+    if (!Files.exists(stackYamlPath)) return util.Collections.emptyList()
+    // If we couldn't parse the stack.yaml, give up.
+    val stackYaml = StackYaml.fromFile(stackYamlPath.toString).valueOr { err =>
+      LOG.info(s"Failed to parse $stackYamlPath: $err")
+      return util.Collections.emptyList()
     }
-    // Quick and dirty way to guess at what files might be config files.
-    // Could do this more cleverly by caching the PackageConfigAssoc values
-    // or something.
-    Files.walk(Paths.get(projectPath)).forEach { path =>
-      val fileName = path.getFileName.toString
-      if (fileName == "stack.yaml" || fileName == "package.yaml") {
-        files.add(path.toFile)
-      } else if (fileName.endsWith(".cabal")) {
-        files.add(path.toFile)
+
+    // Holds onto any files that should be considered external project config files.
+    // This will be the stack.yaml file as well as package.yaml or .cabal files.
+    val files = new util.ArrayList[File]()
+    files.add(stackYamlPath.toFile)
+    val stackYamlDirPath = stackYamlPath.getParent
+    // Check the packages declared in the stack.yaml for a
+    // package.yaml or .cabal file.
+    stackYaml.packages.forEach { pkg =>
+      val pkgPath = stackYamlDirPath.resolve(pkg.path)
+      val packageYamlFile = pkgPath.resolve("package.yaml").toFile
+      // If the package.yaml exists, we want to consider it a config file.
+      // Prefer a package.yaml to a .cabal file for a given package.
+      if (packageYamlFile.exists()) {
+        files.add(packageYamlFile)
+      } else {
+        // Only check the cabal file if the package.yaml does not exist.
+        // This is important to do since re-importing a project will rewrite
+        // the cabal file. If we didn't do this check, the rewrite would
+        // trigger us to suggest to re-import again, which we don't want.
+        pkgPath.toFile.listFiles(
+          (_, name) => name.endsWith(".cabal")
+        ).foreach { cabalFile =>
+          files.add(cabalFile)
+        }
       }
       ()
     }
