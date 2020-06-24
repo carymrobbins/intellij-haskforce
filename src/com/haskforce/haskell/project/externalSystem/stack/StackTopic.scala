09e5bc3fb49863093d015c5b9fc6cbea109b69fa
Cary Robbins <carymrobbins@gmail.com>
2020-06-24 13:57:21 -0500

    [stack-manager] Major victory: reload icon appearing and working when modifying stack.yaml

--- src/com/haskforce/haskell/project/externalSystem/stack/StackTopic.scala
+++ src/com/haskforce/haskell/project/externalSystem/stack/StackTopic.scala
@@ -4,4 +4,7 @@ import com.intellij.util.messages.Topic
 
 object StackTopic extends Topic[StackProjectSettingsListener](
   "Stack project settings", classOf[StackProjectSettingsListener]
-)
+) {
+  // Useful for calling from Java.
+  def get(): StackTopic.type = this
+}
