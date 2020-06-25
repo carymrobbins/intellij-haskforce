0f163822bf40b5d89cd5b4b9445aefc60f2d204c
Cary Robbins <carymrobbins@gmail.com>
2020-06-24 20:13:56 -0500

    [stack-manager] Simplify StackExecutionSettings and add an explicit doc

--- src/com/haskforce/haskell/project/externalSystem/stack/StackExecutionSettings.scala
+++ src/com/haskforce/haskell/project/externalSystem/stack/StackExecutionSettings.scala
@@ -2,28 +2,31 @@ package com.haskforce.haskell.project.externalSystem.stack
 
 import com.intellij.openapi.externalSystem.model.settings.ExternalSystemExecutionSettings
 
-// Note: All of the fields must be marshallable. For example, you cannot
-// add a 'Project' field since 'ProjectImpl' is not marshallable..
+/**
+ * Contains the settings passed from the IDE to the external system
+ * process which will perform the auto-import. We pre-compute the
+ * [[packageConfigAssocs]] and supply it here because the external
+ * system process cannot access IDE features such as PSI parsing, which
+ * is something we need in order to inspect the Cabal and YAML files.
+ *
+ * We could, instead, run the auto-import in the IDE process by updating the
+ * HASKELL_STACK.system.in.process value in our plugin.xml to be "true";
+ * however, it's not clear that would really buy us much. Will likely only
+ * do a refactor like that if there's a very clear benefit.
+ *
+ * Note: All of the fields must be marshallable so the instance can be
+ * serialized and sent to the external system process. For example, you cannot
+ * add a 'Project' field since 'ProjectImpl' is not marshallable. If you
+ * mess this up, you will end up with an exception in the Build
+ * console stating:
+ *
+ *    error marshalling arguments; nested exception is:
+ *        java.io.NotSerializableException: path.to.Class
+ */
 final case class StackExecutionSettings(
   linkedProjectPath: String,
   stackExePath: String,
   stackYamlPath: String,
   rootProjectName: String,
-  // TODO: Does this need to be Array? List or Vector would be nice to
-  // get a derived equals() method.
   packageConfigAssocs: Array[PackageConfigAssoc]
-) extends ExternalSystemExecutionSettings {
-
-  override def equals(o: Any): Boolean = {
-    o match {
-      case x: StackExecutionSettings =>
-        (
-          linkedProjectPath == x.linkedProjectPath
-            && stackExePath == x.stackExePath
-            && stackYamlPath == x.stackYamlPath
-            && rootProjectName == x.rootProjectName
-            && packageConfigAssocs.toVector == x.packageConfigAssocs.toVector
-        )
-    }
-  }
-}
+) extends ExternalSystemExecutionSettings
