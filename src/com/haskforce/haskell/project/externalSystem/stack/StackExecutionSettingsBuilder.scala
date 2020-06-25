a9349c892d4b0895911f03a64e6204d70cc2b7ec
Cary Robbins <carymrobbins@gmail.com>
2020-06-24 20:23:52 -0500

    [stack-manager] Refactor StackExecutionSettingsBuilder

--- src/com/haskforce/haskell/project/externalSystem/stack/StackExecutionSettingsBuilder.scala
+++ src/com/haskforce/haskell/project/externalSystem/stack/StackExecutionSettingsBuilder.scala
@@ -16,182 +16,20 @@ import prelude._
 
 import scala.util.control.NonFatal
 
-class StackExecutionSettingsBuilder(
-  projectPath: String,
-  stackExePath: String,
-  stackYamlPath: String
-) {
-
-  import StackExecutionSettingsBuilder._
-
-  def create(): StackExecutionSettings = {
-    stackRegenCabalFiles()
-    val rootProjectName = inferRootProjectName(projectPath)
-    val packageConfigAssocs = stackIterCabalFilePaths().map { path =>
-      val file = new File(path)
-      val packageDir = file.getParentFile.getCanonicalPath
-      val packageConfig = parsePackageConfig(file)
-      PackageConfigAssoc(
-        packageDir = packageDir,
-        packageConfig = packageConfig
-      )
-    }.toArray
-    StackExecutionSettings(
-      linkedProjectPath = projectPath,
-      stackExePath = stackExePath,
-      stackYamlPath = stackYamlPath,
-      rootProjectName = rootProjectName,
-      packageConfigAssocs = packageConfigAssocs
-    )
-  }
-
-  private def stackRegenCabalFiles(): Unit = {
-    val c = new GeneralCommandLine(
-      stackExePath, "--stack-yaml", stackYamlPath,
-      "build", "--dry-run"
-    )
-    c.setWorkDirectory(projectPath)
-    val p = c.createProcess()
-    val timedOut = !p.waitFor(
-      STACK_REGEN_CABAL_FILE_TIMEOUT_MILLIS,
-      TimeUnit.MILLISECONDS
-    )
-    if (timedOut) {
-      LOG.warn(
-        new StackSystemException(
-          "Regenerating cabal files via 'stack' failed to complete within timeout",
-          vars = List(
-            "timeoutMillis" -> STACK_REGEN_CABAL_FILE_TIMEOUT_MILLIS,
-            "commandLine" -> c.getCommandLineString,
-            "workDir" -> projectPath,
-          )
-        )
-      )
-    } else if (p.exitValue() != 0) {
-      val out: String = readInputStreamToStringOrFallback(p.getInputStream)
-      val err: String = readInputStreamToStringOrFallback(p.getErrorStream)
-      LOG.warn(
-        new StackSystemException(
-          "Failed to regenerate cabal files via 'stack'",
-          vars = List(
-            "exitCode" -> p.exitValue(),
-            "commandLine" -> c.getCommandLineString,
-            "workDir" -> projectPath,
-            "stdout" -> s"{\n$out\n}",
-            "stderr" -> s"{\n$err\n}",
-          )
-        )
-      )
-    }
-  }
-
-  private def readInputStreamToStringOrFallback(
-    is: InputStream,
-    fallback: String = "?"
-  ): String = {
-    try {
-      IOUtils.toString(is, StandardCharsets.UTF_8)
-    } catch {
-      case NonFatal(_) => fallback
-    }
-  }
-
-  private def stackIterCabalFilePaths(): Iterator[String] = {
-    val c = new GeneralCommandLine(
-      stackExePath, "--stack-yaml", stackYamlPath,
-      "ide", "packages", "--stdout", "--cabal-files"
-    )
-    c.setWorkDirectory(projectPath)
-    val p = c.createProcess()
-    val timedOut = !p.waitFor(
-      STACK_GET_CABAL_FILES_TIMEOUT_MILLIS,
-      TimeUnit.MILLISECONDS
-    )
-    if (timedOut) {
-      throw new StackSystemException(
-        "Getting cabal files via 'stack' failed to complete within timeout",
-        vars = List(
-          "timeoutMillis" -> STACK_GET_CABAL_FILES_TIMEOUT_MILLIS,
-          "commandLine" -> c.getCommandLineString,
-        )
-      )
-    }
-    if (p.exitValue() != 0) {
-      val err = IOUtils.toString(p.getErrorStream, StandardCharsets.UTF_8)
-      throw new StackSystemException(
-        "Failed to get cabal files via 'stack'",
-        vars = List(
-          "exitCode" -> p.exitValue(),
-          "commandLine" -> c.getCommandLineString,
-          "stderr" -> err,
-        )
-      )
-    }
-    inputStreamIterLines(p.getInputStream)
-  }
-
-  private def inputStreamIterLines(is: InputStream): Iterator[String] = {
-    val r = new BufferedReader(new InputStreamReader(is))
-    Iterator.continually(r.readLine()).takeWhile(_ != null)
-  }
-
-  private def parsePackageConfig(file: File): PackageConfig = {
-    PackageConfig.fromFile(file) match {
-      case Right(Some(packageConfig)) => packageConfig
-      case Right(None) =>
-        throw new StackSystemException(
-          "Invalid package config file",
-          vars = List("file" -> file)
-        )
-      case Left(e) =>
-        throw new StackSystemException(
-          "Failed to parse package config from cabal file",
-          cause = e,
-          vars = List("file" -> file)
-        )
-    }
-  }
-
-  private def inferRootProjectName(projectPath: String): String = {
-    val root = new File(projectPath)
-    val rootPackageYaml = new File(root, "package.yaml")
-    if (!rootPackageYaml.exists()) {
-      if (!root.isDirectory) {
-        throw new StackSystemException(
-          "Failed to infer root project name; project path points to non-directory",
-          vars = List(
-            "projectPath" -> projectPath,
-          )
-        )
-      }
-      return root.getName
-    }
-    PsiFileParser.parseForDefaultProject[YAMLFile, File](rootPackageYaml)
-      .flatMap(PackageYamlQuery.getName)
-      .valueOr { e =>
-        throw new StackSystemException(
-          "Failed to infer root project name; failed to parse 'name' from root package.yaml",
-          cause = e,
-          vars = List(
-            "rootPackageYaml" -> rootPackageYaml,
-          )
-        )
-      }
-  }
-}
-
+/** Builder to produce a [[StackExecutionSettings]] for a given project. */
 object StackExecutionSettingsBuilder {
 
-  // 10 seconds should be plenty long enough.
-  private val STACK_REGEN_CABAL_FILE_TIMEOUT_MILLIS: Long = 10000
-  private val STACK_GET_CABAL_FILES_TIMEOUT_MILLIS: Long = 10000
-
-  private val LOG = Logger.getInstance(classOf[StackExecutionSettingsBuilder])
-
-  def forProject(
+  def buildForProject(
     project: Project,
     projectPath: String
-  ): StackExecutionSettingsBuilder = {
+  ): StackExecutionSettings = {
+    mk(project, projectPath).build()
+  }
+
+  private def mk(
+    project: Project,
+    projectPath: String
+  ): Builder = {
     val haskellBuildSettings = HaskellBuildSettings.getInstance(project)
     val stackExePath =
       Option(haskellBuildSettings.getStackPath)
@@ -223,10 +61,178 @@ object StackExecutionSettingsBuilder {
             vars = List("project" -> project)
           )
         }
-    new StackExecutionSettingsBuilder(
+    new Builder(
       projectPath = projectPath,
       stackExePath = stackExePath,
       stackYamlPath = stackYamlPath
     )
+  }
+
+  // 10 seconds should be plenty long enough.
+  private val STACK_REGEN_CABAL_FILE_TIMEOUT_MILLIS: Long = 10000
+  private val STACK_GET_CABAL_FILES_TIMEOUT_MILLIS: Long = 10000
+
+  private val LOG = Logger.getInstance(classOf[Builder])
+
+  private class Builder(
+    projectPath: String,
+    stackExePath: String,
+    stackYamlPath: String
+  ) {
+
+    def build(): StackExecutionSettings = {
+      stackRegenCabalFiles()
+      val rootProjectName = inferRootProjectName(projectPath)
+      val packageConfigAssocs = stackIterCabalFilePaths().map { path =>
+        val file = new File(path)
+        val packageDir = file.getParentFile.getCanonicalPath
+        val packageConfig = parsePackageConfig(file)
+        PackageConfigAssoc(
+          packageDir = packageDir,
+          packageConfig = packageConfig
+        )
+      }.toArray
+      StackExecutionSettings(
+        linkedProjectPath = projectPath,
+        stackExePath = stackExePath,
+        stackYamlPath = stackYamlPath,
+        rootProjectName = rootProjectName,
+        packageConfigAssocs = packageConfigAssocs
+      )
+    }
+
+    private def stackRegenCabalFiles(): Unit = {
+      val c = new GeneralCommandLine(
+        stackExePath, "--stack-yaml", stackYamlPath,
+        "build", "--dry-run"
+      )
+      c.setWorkDirectory(projectPath)
+      val p = c.createProcess()
+      val timedOut = !p.waitFor(
+        STACK_REGEN_CABAL_FILE_TIMEOUT_MILLIS,
+        TimeUnit.MILLISECONDS
+      )
+      if (timedOut) {
+        LOG.warn(
+          new StackSystemException(
+            "Regenerating cabal files via 'stack' failed to complete within timeout",
+            vars = List(
+              "timeoutMillis" -> STACK_REGEN_CABAL_FILE_TIMEOUT_MILLIS,
+              "commandLine" -> c.getCommandLineString,
+              "workDir" -> projectPath,
+            )
+          )
+        )
+      } else if (p.exitValue() != 0) {
+        val out: String = readInputStreamToStringOrFallback(p.getInputStream)
+        val err: String = readInputStreamToStringOrFallback(p.getErrorStream)
+        LOG.warn(
+          new StackSystemException(
+            "Failed to regenerate cabal files via 'stack'",
+            vars = List(
+              "exitCode" -> p.exitValue(),
+              "commandLine" -> c.getCommandLineString,
+              "workDir" -> projectPath,
+              "stdout" -> s"{\n$out\n}",
+              "stderr" -> s"{\n$err\n}",
+            )
+          )
+        )
+      }
+    }
+
+    private def readInputStreamToStringOrFallback(
+      is: InputStream,
+      fallback: String = "?"
+    ): String = {
+      try {
+        IOUtils.toString(is, StandardCharsets.UTF_8)
+      } catch {
+        case NonFatal(_) => fallback
+      }
+    }
+
+    private def stackIterCabalFilePaths(): Iterator[String] = {
+      val c = new GeneralCommandLine(
+        stackExePath, "--stack-yaml", stackYamlPath,
+        "ide", "packages", "--stdout", "--cabal-files"
+      )
+      c.setWorkDirectory(projectPath)
+      val p = c.createProcess()
+      val timedOut = !p.waitFor(
+        STACK_GET_CABAL_FILES_TIMEOUT_MILLIS,
+        TimeUnit.MILLISECONDS
+      )
+      if (timedOut) {
+        throw new StackSystemException(
+          "Getting cabal files via 'stack' failed to complete within timeout",
+          vars = List(
+            "timeoutMillis" -> STACK_GET_CABAL_FILES_TIMEOUT_MILLIS,
+            "commandLine" -> c.getCommandLineString,
+          )
+        )
+      }
+      if (p.exitValue() != 0) {
+        val err = IOUtils.toString(p.getErrorStream, StandardCharsets.UTF_8)
+        throw new StackSystemException(
+          "Failed to get cabal files via 'stack'",
+          vars = List(
+            "exitCode" -> p.exitValue(),
+            "commandLine" -> c.getCommandLineString,
+            "stderr" -> err,
+          )
+        )
+      }
+      inputStreamIterLines(p.getInputStream)
+    }
+
+    private def inputStreamIterLines(is: InputStream): Iterator[String] = {
+      val r = new BufferedReader(new InputStreamReader(is))
+      Iterator.continually(r.readLine()).takeWhile(_ != null)
+    }
+
+    private def parsePackageConfig(file: File): PackageConfig = {
+      PackageConfig.fromFile(file) match {
+        case Right(Some(packageConfig)) => packageConfig
+        case Right(None) =>
+          throw new StackSystemException(
+            "Invalid package config file",
+            vars = List("file" -> file)
+          )
+        case Left(e) =>
+          throw new StackSystemException(
+            "Failed to parse package config from cabal file",
+            cause = e,
+            vars = List("file" -> file)
+          )
+      }
+    }
+
+    private def inferRootProjectName(projectPath: String): String = {
+      val root = new File(projectPath)
+      val rootPackageYaml = new File(root, "package.yaml")
+      if (!rootPackageYaml.exists()) {
+        if (!root.isDirectory) {
+          throw new StackSystemException(
+            "Failed to infer root project name; project path points to non-directory",
+            vars = List(
+              "projectPath" -> projectPath,
+            )
+          )
+        }
+        return root.getName
+      }
+      PsiFileParser.parseForDefaultProject[YAMLFile, File](rootPackageYaml)
+        .flatMap(PackageYamlQuery.getName)
+        .valueOr { e =>
+          throw new StackSystemException(
+            "Failed to infer root project name; failed to parse 'name' from root package.yaml",
+            cause = e,
+            vars = List(
+              "rootPackageYaml" -> rootPackageYaml,
+            )
+          )
+        }
+    }
   }
 }
