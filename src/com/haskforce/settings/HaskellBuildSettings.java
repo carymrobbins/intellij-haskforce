d33f6e31b07c3ce0e94aeeb2d1f8eebddad7e1e7
Cary Robbins <carymrobbins@gmail.com>
2020-06-24 19:57:19 -0500

    [stack-manager] Minor cleanups; remove println, improve comments

--- src/com/haskforce/settings/HaskellBuildSettings.java
+++ src/com/haskforce/settings/HaskellBuildSettings.java
@@ -32,11 +32,6 @@ public class HaskellBuildSettings implements PersistentStateComponent<HaskellBui
         myBuildOptions = state;
     }
 
-    @Override
-    public void initializeComponent() {
-        System.out.println("HaskellBuildSettings.initializeComponent");
-    }
-
     public boolean isCabalEnabled() {
         return myBuildOptions.myUseCabal;
     }
