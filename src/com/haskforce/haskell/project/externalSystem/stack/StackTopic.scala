a9d349612362319fc2d650b50d75d8df47b5a321
Cary Robbins <carymrobbins@gmail.com>
2020-06-24 19:00:36 -0500

    [stack-manager] Use StackSettings.scala over the .java version

--- src/com/haskforce/haskell/project/externalSystem/stack/StackTopic.scala
+++ src/com/haskforce/haskell/project/externalSystem/stack/StackTopic.scala
@@ -4,7 +4,4 @@ import com.intellij.util.messages.Topic
 
 object StackTopic extends Topic[StackProjectSettingsListener](
   "Stack project settings", classOf[StackProjectSettingsListener]
-) {
-  // Useful for calling from Java.
-  def get(): StackTopic.type = this
-}
+)
