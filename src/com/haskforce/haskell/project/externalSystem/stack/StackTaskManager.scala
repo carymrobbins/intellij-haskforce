83746b9db1d8f0ec7758129f6fed1b74502ca356
Cary Robbins <carymrobbins@gmail.com>
2020-06-26 09:13:41 -0500

    [stack-manager] StackTaskManager doc comment

--- src/com/haskforce/haskell/project/externalSystem/stack/StackTaskManager.scala
+++ src/com/haskforce/haskell/project/externalSystem/stack/StackTaskManager.scala
@@ -5,6 +5,27 @@ import java.util
 import com.intellij.openapi.externalSystem.model.task.{ExternalSystemTaskId, ExternalSystemTaskNotificationListener}
 import com.intellij.openapi.externalSystem.task.ExternalSystemTaskManager
 
+/**
+ * Currently, this task manager does nothing.
+ *
+ * Once we're ready, here's vaguely what we'll need to do.
+ *
+ * Update [[StackProjectResolver]], and thus, [[StackProjectDataNodeBuilder]],
+ * to construct [[com.intellij.openapi.externalSystem.model.task.TaskData]]
+ * nodes and insert them into the built tree.
+ *
+ * I think these tasks should appear in the build tool window (which as of this
+ * writing does not yet exist). When developing HaskForce, you should see a
+ * 'Gradle' tool window available, usually on the right panel. We would
+ * likely need to build this out as well for 'Stack'.
+ *
+ * We'll need to create an subclass of
+ * [[com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunConfiguration]].
+ * See [[org.jetbrains.plugins.gradle.service.execution.GradleRunConfiguration]]
+ * as an example.
+ *
+ * There's probably more involved that I'm not yet aware of, so good luck.
+ */
 final class StackTaskManager
   extends ExternalSystemTaskManager[StackExecutionSettings] {
 
@@ -16,14 +37,12 @@ final class StackTaskManager
     jvmAgentSetup: String,
     listener: ExternalSystemTaskNotificationListener
   ): Unit = {
-    // TODO
   }
 
   override def cancelTask(
     id: ExternalSystemTaskId,
     listener: ExternalSystemTaskNotificationListener
   ): Boolean = {
-    // TODO
     false
   }
 }
