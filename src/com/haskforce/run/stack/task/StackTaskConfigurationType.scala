e0e3ff0ce7099d76969af4141c9cb15326639832
Cary Robbins <carymrobbins@gmail.com>
2022-05-03 05:45:03 -0500

    Fix PluginException in StackTaskConfigurationType
    com.intellij.diagnostic.PluginException: The default implementation of method 'getId' is deprecated, you need to override it in 'class com.haskforce.run.stack.task.StackTaskConfigurationType$$anon$1'. The default implementation delegates to 'getName' which may be localized, but return value of this method must not depend on current localization. [Plugin: com.haskforce]

--- src/com/haskforce/run/stack/task/StackTaskConfigurationType.scala
+++ src/com/haskforce/run/stack/task/StackTaskConfigurationType.scala
@@ -4,9 +4,17 @@ import com.haskforce.HaskellIcons
 import com.intellij.execution.configurations.{ConfigurationFactory, ConfigurationTypeBase, RunConfiguration}
 import com.intellij.openapi.project.Project
 
-class StackTaskConfigurationType extends ConfigurationTypeBase("Stack Task Configuration", "Haskell Stack Task", "Execute a `stack` task.", HaskellIcons.FILE) {
+class StackTaskConfigurationType extends ConfigurationTypeBase(
+  "Stack Task Configuration",
+  "Haskell Stack Task",
+  "Execute a `stack` task.",
+  HaskellIcons.FILE
+) {
 
-  addFactory(new ConfigurationFactory(this){
+  addFactory(new ConfigurationFactory(this) {
+
+    override def getId: String = "haskell-stack-task"
+
     override def createTemplateConfiguration(project: Project): RunConfiguration = {
       new StackTaskConfiguration(project, this)
     }
